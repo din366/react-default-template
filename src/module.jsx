7798c9996d12609823ab080ddd0dcaeb4e5aec0b
Alex Din <din366@gmail.com>
2022-05-20 02:06:49 +0300

    push default template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import picture from './img/picture.png'
+
+const Module = () => (
+  <>
+    <h1>Hello React</h1>
+    <img src={picture}/>
+  </>
+);
+
+export default Module;
